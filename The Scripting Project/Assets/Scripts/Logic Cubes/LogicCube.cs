fd6283b05c9e2546bd4b96bac19581fc0ce8795b
Daniel Everland <danieleverland@gmail.com>
2019-08-18 01:57:48 +0200

    Made nodes and logic cubes non-serializable

--- The Scripting Project/Assets/Scripts/Logic Cubes/LogicCube.cs
+++ The Scripting Project/Assets/Scripts/Logic Cubes/LogicCube.cs
@@ -3,12 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
 
-[System.Serializable]
 public class LogicCube : ILogicCube
 {
-    [JsonIgnore]
     public DataContainer Data { get { return data; } }
 
-    [JsonProperty]
     private DataContainer data = new DataContainer();
 }
--- The Scripting Project/Assets/Scripts/Nodes/Node.cs
+++ The Scripting Project/Assets/Scripts/Nodes/Node.cs
@@ -3,12 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
 
-[System.Serializable]
 public class Node : INode
 {
-    [JsonIgnore]
     public DataContainer Data { get { return data; } }
 
-    [JsonProperty]
     private DataContainer data = new DataContainer();
 }
