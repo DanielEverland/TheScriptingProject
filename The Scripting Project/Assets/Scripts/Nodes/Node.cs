b2634dea19c913e89029363c563c24c5c8022f4f
Daniel Everland <danieleverland@gmail.com>
2019-08-18 01:56:03 +0200

    Created base classes

---
+++ The Scripting Project/Assets/Scripts/Nodes/Node.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+[System.Serializable]
+public class Node : INode
+{
+    [JsonIgnore]
+    public DataContainer Data { get { return data; } }
+
+    [JsonProperty]
+    private DataContainer data = new DataContainer();
+}
