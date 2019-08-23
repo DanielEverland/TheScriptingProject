38482eaa996c7e37464d9052810c09cff4e4131b
Daniel Everland (Laptop) <danieleverland@gmail.com>
2019-08-23 09:39:30 +0200

    Removed quaternion from base node

--- The Scripting Project/Assets/Scripts/Nodes/Node.cs
+++ The Scripting Project/Assets/Scripts/Nodes/Node.cs
@@ -8,11 +8,7 @@ public class Node : INode
 {
     [JsonIgnore]
     public Vector2 Position { get => position; set => position = value; }
-    [JsonIgnore]
-    public Quaternion Rotation { get => rotation; set => rotation = value; }
 
     [JsonProperty]
     private Vector2 position;
-    [JsonProperty]
-    private Quaternion rotation;
 }
